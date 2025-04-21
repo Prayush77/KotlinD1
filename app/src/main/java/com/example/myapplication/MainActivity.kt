17ab9516b42560fad1bb670ac00346702a17cd98
Prayush Gurung <prayushtamugurung@gmail.com>
2025-04-21 21:43:00 +0545

    initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            MainHeading()
+        }
+
+    }
+}
+@Composable
+
+fun MainHeading(){
+
+    Scaffold { innerPadding->
+        Row (
+            modifier = Modifier.padding(innerPadding))
+        {
+
+            Text(text = "Hello world")
+            Text(text = "Hello world")
+        }
+    }
+
+}
+
+@Composable
+fun MainBody(){
+
+
+}
+
+@Composable
+fun MainFooter(){
+
+}
---
+++ app/src/main/java/com/example/myapplication/lesson3.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication
+
+fun main(args: Array<String>) {
+    print("HEllo world")
+
+// mutable
+// we can reassign values to mutabole varibales
+
+    var age = 35;
+
+    age = 25
+
+
+// immutable
+// we can't re assign values to immutable variables
+
+    val roll = 15
+
+//    roll = 90// we cannot reassign
+
+    var a: Boolean = true
+    var b: Char ='R'
+    var c: Byte = 12
+    var d: Short = -356
+    var e: Int = 4536
+    var f: Long=-512576152L
+    var i: Float = 5.23424F
+    var h: Double = 7.2342342
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(i)
+    println(h)
+
+    //Type conversion in kotlin
+
+    var x : Double = 132.66
+    var y : Int = x.toInt()
+    var z : Byte = y.toByte()
+
+    println(x)
+    println(y)
+    println(z)
+
+    var greeting: String = "Hello world"
+    var length: Int= greeting.length
+    var isEqual: Boolean = a.equals("Hello world")
+    var username: String = " softwarica "
+    println(username.trim())
+    println(greeting);
+    println(length)
+    println(greeting.isEmpty())
+    println(greeting.lowercase())
+    println(greeting.uppercase())
+    println(x)
+    print(greeting.plus(", How are you?"));
+
+
+}
